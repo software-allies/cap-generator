24f038462a1ea2c500ceeed806ea751c7bcdee03
Cristofer Flores <cristofer.flort@gmail.com>
2018-07-23 15:19:49 -0500

    Add the first prompts to the generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,32 +7,49 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the super-excellent ${chalk.red('generator-cap')} generator!`)
+      yosay(`${chalk.red('Connected Apps Platform Generartor\n Build amazing apps faster and better')}`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'list',
+        name: 'appType',
+        message: 'What type of app are you going to build?',
+        choices: [
+          {
+            name: `api (Using ${chalk.red('Loopback')})`,
+            value: 'api'
+          },
+          {
+            name: `client (Using ${chalk.red('Ionic')})`,
+            value: 'client'
+          }
+        ]
+      },
+      {
+        type: 'checkbox',
+        name: 'client.modules',
+        message: 'Select the modules you want to include',
+        choices: [
+          {
+            name: 'ionic-auth-module',
+            value: 'ionoic-auth',
+            short: 'Ionic Auth Module'
+          },
+          {
+            name: 'ionic-storage',
+            value: 'ionoic-storage',
+            short: 'Ionic Storage Module'
+          } 
+        ],
+        when: (ctx) => ctx.appType === 'client'
       }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      console.log('this.props', this.props)
     });
-  }
-
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
   }
 };
