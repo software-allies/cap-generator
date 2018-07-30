070229e46df70b457555a9e056905e8b113e9000
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-30 09:22:51 -0500

    add the template cap-auth-module, which creates three pages, login, re..

---
+++ generators/cap-auth-module/pages/login/login.ts
@@ -0,0 +1,50 @@
+import { HomePage } from './../home/home';
+import { RegisterPage } from './../register/register';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: '<%= name %>-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private toastCtrl: ToastController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  presentToast(text: string) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present();
+  }
+
+  onSubmit(result: any) {
+    if (result.id) {
+        this.presentToast('Register Successfull!');
+        this.navCtrl.push(HomePage);
+    }
+  }
+
+  onChangePage(changePage: boolean) {
+        this.navCtrl.push(RegisterPage);
+  }
+}
