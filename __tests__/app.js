cd5ee1e5c77df74806b9f17c359e65615762a477
Cristofer Flores <cristofer.flort@gmail.com>
2018-08-06 12:09:23 -0500

    Add more test for the client

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,25 +4,25 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('[API] - generator-cap:app', () => {
-  this.files = [
-    'test/server',
-    'test/server/boot',
-    'test/server/boot/authentication.js',
-    'test/server/boot/root.js',
-    'test/server/component-config.json',
-    'test/server/config.json',
-    'test/server/datasources.json',
-    'test/server/middleware.development.json',
-    'test/server/middleware.json',
-    'test/server/model-config.json',
-    'test/package.json',
-    'test/README.md'
+  const files = [
+    'apiTest/server',
+    'apiTest/server/boot',
+    'apiTest/server/boot/authentication.js',
+    'apiTest/server/boot/root.js',
+    'apiTest/server/component-config.json',
+    'apiTest/server/config.json',
+    'apiTest/server/datasources.json',
+    'apiTest/server/middleware.development.json',
+    'apiTest/server/middleware.json',
+    'apiTest/server/model-config.json',
+    'apiTest/package.json',
+    'apiTest/README.md'
   ]
 
   beforeAll(() => {
     const answers = {
       type: 'api',
-      name: 'test',
+      name: 'apiTest',
       modules: []
     }
     return helpers
@@ -31,6 +31,45 @@ describe('[API] - generator-cap:app', () => {
   });
 
   it('Should create all the files needed for the API', () => {
-    assert.file(this.files);
+    assert.file(files);
+  });
+});
+
+describe('[CLIENT] - generator-cap:app', () => {
+  const files = [
+    'clientTest/resources',
+    'clientTest/resources/android',
+    'clientTest/resources/ios',
+    'clientTest/resources/icon.png',
+    'clientTest/resources/README.md',
+    'clientTest/resources/splash.png',
+    'clientTest/src',
+    'clientTest/src/app',
+    'clientTest/src/assets',
+    'clientTest/src/pages',
+    'clientTest/src/theme',
+    'clientTest/src/index.html',
+    'clientTest/src/manifest.json',
+    'clientTest/src/service-worker.js',
+    'clientTest/config.xml',
+    'clientTest/ionic.config.json',
+    'clientTest/package.json',
+    'clientTest/tsconfig.json',
+    'clientTest/tslint.json'
+  ]
+
+  beforeAll(() => {
+    const answers = {
+      type: 'client',
+      name: 'clientTest',
+      modules: []
+    }
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers);
+  });
+
+  it('Should create all the files needed for the CLIENT app without extra modules', () => {
+    assert.file(files);
   });
 });
