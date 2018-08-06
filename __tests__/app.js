a439c2bc0f4e0c0bde01e1a3c08c846ec1cb19f4
Cristofer Flores <cristofer.flort@gmail.com>
2018-08-06 17:03:52 -0500

    Add the unit test for the main generator

--- __tests__/app.js
+++ __tests__/app.js
@@ -36,40 +36,167 @@ describe('[API] - generator-cap:app', () => {
 });
 
 describe('[CLIENT] - generator-cap:app', () => {
+  const appName = 'clientTest'
+
   const files = [
-    'clientTest/resources',
-    'clientTest/resources/android',
-    'clientTest/resources/ios',
-    'clientTest/resources/icon.png',
-    'clientTest/resources/README.md',
-    'clientTest/resources/splash.png',
-    'clientTest/src',
-    'clientTest/src/app',
-    'clientTest/src/assets',
-    'clientTest/src/pages',
-    'clientTest/src/theme',
-    'clientTest/src/index.html',
-    'clientTest/src/manifest.json',
-    'clientTest/src/service-worker.js',
-    'clientTest/config.xml',
-    'clientTest/ionic.config.json',
-    'clientTest/package.json',
-    'clientTest/tsconfig.json',
-    'clientTest/tslint.json'
+    `${appName}/resources`,
+    `${appName}/resources/android`,
+    `${appName}/resources/ios`,
+    `${appName}/resources/icon.png`,
+    `${appName}/resources/README.md`,
+    `${appName}/resources/splash.png`,
+    `${appName}/src`,
+    `${appName}/src/app`,
+    `${appName}/src/assets`,
+    `${appName}/src/pages`,
+    `${appName}/src/theme`,
+    `${appName}/src/index.html`,
+    `${appName}/src/manifest.json`,
+    `${appName}/src/service-worker.js`,
+    `${appName}/config.xml`,
+    `${appName}/ionic.config.json`,
+    `${appName}/package.json`,
+    `${appName}/tsconfig.json`,
+    `${appName}/tslint.json`
   ]
 
-  beforeAll(() => {
+  const authModuleFiles = [
+    `${appName}/src/pages/change-password`,
+    `${appName}/src/pages/change-password/change-password.html`,
+    `${appName}/src/pages/change-password/change-password.module.ts`,
+    `${appName}/src/pages/change-password/change-password.scss`,
+    `${appName}/src/pages/change-password/change-password.ts`,
+    `${appName}/src/pages/login/login.html`,
+    `${appName}/src/pages/login/login.module.ts`,
+    `${appName}/src/pages/login/login.scss`,
+    `${appName}/src/pages/login/login.ts`,
+    `${appName}/src/pages/register/register.html`,
+    `${appName}/src/pages/register/register.module.ts`,
+    `${appName}/src/pages/register/register.scss`,
+    `${appName}/src/pages/register/register.ts`
+  ]
+
+  const awsModuleFiles = [
+    `${appName}/src/pages/photo-list`,
+    `${appName}/src/pages/photo-list/photo-list.html`,
+    `${appName}/src/pages/photo-list/photo-list.module.ts`,
+    `${appName}/src/pages/photo-list/photo-list.scss`,
+    `${appName}/src/pages/photo-list/photo-list.ts`,
+    `${appName}/src/pages/photo-upload/photo-upload.html`,
+    `${appName}/src/pages/photo-upload/photo-upload.module.ts`,
+    `${appName}/src/pages/photo-upload/photo-upload.scss`,
+    `${appName}/src/pages/photo-upload/photo-upload.ts`
+  ]
+  
+  it('Should create all the files needed for the CLIENT app without extra modules', (done) => {
     const answers = {
       type: 'client',
-      name: 'clientTest',
+      name: appName,
       modules: []
     }
-    return helpers
+
+    helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts(answers);
+      .withPrompts(answers)
+      .then(dir => {
+        assert.file(files);
+        assert.noFile(authModuleFiles);
+        assert.noFile(awsModuleFiles);
+        done()
+      })
   });
 
-  it('Should create all the files needed for the CLIENT app without extra modules', () => {
-    assert.file(files);
+  it('Should create and configure all the files for the client app and the Auth Module only.', (done) => {
+    const answers = {
+      type: 'client',
+      name: appName,
+      modules: [{
+        name: 'cap-authorization',
+        version: '0.0.2'
+      }],
+      authApiUrl: 'http:localhost:3000/api',
+      authLoginEndPoint: 'users'
+    }
+    helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+      .then(dir => {
+        assert.file(files);
+        assert.file(authModuleFiles);
+        assert.noFile(awsModuleFiles);
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("apiUrl: '" + answers.authApiUrl + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("loginEndpoint: '" + answers.authLoginEndPoint + "'"));
+        done()
+      })
+  });
+
+  it('Should create and configure all the files for the client app and AWS Storage Module only.', (done) => {
+    const answers = {
+      type: 'client',
+      name: appName,
+      modules: [{
+        name: 'cap-storage-aws',
+        version: '1.0.1'
+      }],
+      awsBucket: 'testAwsBucket',
+      awsAccessKeyId: 'testAwsKey',
+      awsSecretAccessKey: 'testAwsSecretKey',
+      awsRegion: 'testAwsRegion',
+      awsFolder: 'testAwsFolder'
+    }
+    helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+      .then(dir => {
+        assert.file(files);
+        assert.file(awsModuleFiles);
+        assert.noFile(authModuleFiles);
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("bucket: '" + answers.awsBucket + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("accessKeyId: '" + answers.awsAccessKeyId + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("secretAccessKey: '" + answers.awsSecretAccessKey + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("region: '" + answers.awsRegion + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("folder: '" + answers.awsFolder + "'"));
+        done()
+      })
+  });
+
+  it('Should create all the files for the client app, AWS Storage Module and Auth Module.', (done) => {
+    const answers = {
+      type: 'client',
+      name: appName,
+      modules: [
+        {
+          name: 'cap-storage-aws',
+          version: '1.0.1'
+        },
+        {
+          name: 'cap-authorization',
+          version: '0.0.2'
+        }
+      ],
+      authApiUrl: 'http:localhost:3000/api',
+      authLoginEndPoint: 'users',
+      awsBucket: 'testAwsBucket',
+      awsAccessKeyId: 'testAwsKey',
+      awsSecretAccessKey: 'testAwsSecretKey',
+      awsRegion: 'testAwsRegion',
+      awsFolder: 'testAwsFolder'
+    }
+    helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+      .then(dir => {
+        assert.file(files);
+        assert.file(awsModuleFiles);
+        assert.file(authModuleFiles);
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("apiUrl: '" + answers.authApiUrl + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("loginEndpoint: '" + answers.authLoginEndPoint + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("bucket: '" + answers.awsBucket + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("accessKeyId: '" + answers.awsAccessKeyId + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("secretAccessKey: '" + answers.awsSecretAccessKey + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("region: '" + answers.awsRegion + "'"));
+        assert.fileContent(`${appName}/src/app/app.module.ts`, new RegExp("folder: '" + answers.awsFolder + "'"));
+        done()
+      })
   });
 });
