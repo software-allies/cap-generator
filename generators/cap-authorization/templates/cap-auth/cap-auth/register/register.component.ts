d0f099784e616372c5a812d823deb3f17f7817d2
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-10-31 09:41:38 -0600

    CAP generators/cap-authorization/templates/cap-auth/cap-auth/register/ Create the form control to react to the validations of it.

--- generators/cap-authorization/templates/cap-auth/cap-auth/register/register.component.ts
+++ generators/cap-authorization/templates/cap-auth/cap-auth/register/register.component.ts
@@ -9,8 +9,45 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {}
+  createUserForm: FormGroup;
+  existingUser: Boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.existingUser = false;
+    this.createUserForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(8), this.capitalLetter]),
+      'firstName': new FormControl('', [Validators.required, Validators.minLength(2)]),
+      'lastName': new FormControl('', [Validators.required, Validators.minLength(2)]),
+      'company': new FormControl('', [Validators.required, Validators.minLength(2)]),
+      'termsConditions': new FormControl(false, [this.termsAndConditions]),
+    });
+  }
+
+  ngOnInit() { }
+
+  termsAndConditions(control: FormControl): { [s: string]: boolean } {
+    if (control.value) {
+      return null;
+    }
+    return {
+      termsAndConditions: true
+    };
+  }
+
+  capitalLetter(control: FormControl): { [s: string]: boolean } {
+    const letter = control.value.charAt(0);
+    if (control.value && !(letter === control.value.charAt(0).toUpperCase())) {
+      return {
+        capitalLetter: true
+      };
+    }
+    return null;
+  }
+
+
+
+  createUser() {
+    console.log(this.createUserForm.value);
   }
 }
