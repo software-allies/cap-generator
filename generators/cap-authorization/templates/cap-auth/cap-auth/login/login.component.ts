6ba6c30b606cf8dc16902de77c3f4ed65a822dfc
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-10-31 09:44:56 -0600

    CAP generators/cap-authorization/templates/cap-auth/cap-auth/ Create the user interface on the pages forgot password and login, also validate them forms.

--- generators/cap-authorization/templates/cap-auth/cap-auth/login/login.component.ts
+++ generators/cap-authorization/templates/cap-auth/cap-auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,21 @@ import { Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserForm: FormGroup;
+  userNotValid: Boolean;
+
+  constructor() {
+    this.loginUserForm = new FormGroup({
+      'email': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required])
+    });
+    this.userNotValid = false;
+  }
 
   ngOnInit() { }
+
+  loginUser() {
+    console.log(this.loginUserForm.value);
+  }
 
 }
