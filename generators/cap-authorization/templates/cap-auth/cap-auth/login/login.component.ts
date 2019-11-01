5485e9aa05ba8182609307f505afedaebae05af1
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-01 12:01:24 -0600

    CAP-62 generators/cap-authorization/templates/cap-auth/cap-auth/login/login.component.ts Call the authentication.service when the user wants to log in to connect to the API through a request

--- generators/cap-authorization/templates/cap-auth/cap-auth/login/login.component.ts
+++ generators/cap-authorization/templates/cap-auth/cap-auth/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { FormGroup, FormControl, Validators, } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
+import { isPlatformBrowser } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,11 @@ export class LoginComponent implements OnInit {
   loginUserForm: FormGroup;
   userNotValid: Boolean;
 
-  constructor() {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId,
+  ) {
     this.loginUserForm = new FormGroup({
       'email': new FormControl('', [Validators.required]),
       'password': new FormControl('', [Validators.required])
@@ -22,7 +29,14 @@ export class LoginComponent implements OnInit {
   ngOnInit() { }
 
   loginUser() {
-    console.log(this.loginUserForm.value);
+    this.authenticationService.loginAuth0User(this.loginUserForm.value).subscribe((user: any) => {
+        if (isPlatformBrowser(this.platformId)) {
+          localStorage.setItem('Access_Token', JSON.stringify(user));
+          this.router.navigate(['/']);
+        }
+    }, (error) => {
+      this.userNotValid = true;
+    });
   }
 
 }
