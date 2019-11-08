198e6d93d8bc39a2ad02fe207f6277f1bfc4051c
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-08 14:14:53 -0600

    CAP-93 generators/app/templates/client/src/app/app-routing.module.ts define the routs for each module with lazy loading

--- generators/app/templates/client/src/app/app-routing.module.ts
+++ generators/app/templates/client/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-
-<% if (imports && imports.auth) { -%>
+const routes: Routes = [<%if(imports && imports.auth){%>
   {
     path: 'auth',
-    loadChildren: './modules/cap-auth/cap-auth.module#CapAuthModule' ,
-  },
-<% } -%>
+    loadChildren: './modules/cap-auth/cap-auth.module#CapAuthModule',
+  },<%}%>
+  <% if(imports && imports.awsStorage){%>{
+    path: 'aws',
+    loadChildren: './modules/cap-aws-module/cap-aws.module#CapAwsModule'
+  },<%}%>
 ];
 
 @NgModule({
