0f5810828f4b1253f9ce4809afb1476bbf23d85d
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-21 11:42:09 -0600

    CAP-58 CAP-62 generators/cap-authorization/templates/cap-auth/cap-auth/cap-auth-routing.module.ts

--- generators/cap-authorization/templates/cap-auth/cap-auth/cap-auth-routing.module.ts
+++ generators/cap-authorization/templates/cap-auth/cap-auth/cap-auth-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { ForgotComponent } from './forgot/forgot.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotComponent },
+  { path: 'profile', component : ProfileComponent}
 ];
 
 @NgModule({
