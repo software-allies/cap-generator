b5a349a0e08260ade3484219c62109f469000d6d
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-26 11:36:04 -0600

    CAP  generators/app/templates/client/src/app/app-routing.module.ts

--- generators/app/templates/client/src/app/app-routing.module.ts
+++ generators/app/templates/client/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [<%if(imports && imports.auth){%>
-  {
-    path: 'auth',
-    loadChildren: './modules/cap-auth/cap-auth.module#CapAuthModule',
-  },<%}%>
-  <% if(imports && imports.awsStorage){%>{
-    path: 'aws',
-    loadChildren: './modules/cap-aws-module/cap-aws.module#CapAwsModule'
-  },<%}%>
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
