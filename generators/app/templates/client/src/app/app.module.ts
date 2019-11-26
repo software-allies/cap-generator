077403354879f0596f7cd9094ef4dcdc68a9aed7
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-26 11:58:19 -0600

    CAP generators/app/templates/client/src/app/app.module.ts

--- generators/app/templates/client/src/app/app.module.ts
+++ generators/app/templates/client/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CapAuthModule } from './modules/cap-auth/cap-auth.module';
+<% if (authService === 'firebase') { %>
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';<% } -%>
 <% if (imports && imports.liveChat) { %>
-import { CapLiveChatModule } from './modules/cap-live-chat/cap-live-chat.module';
-<% } -%>
+import { CapLiveChatModule } from './modules/cap-live-chat/cap-live-chat.module';<% } -%>
+<% if(imports && imports.awsStorage){%>
+import { CapAwsModule } from './modules/cap-aws-module/cap-aws.module'<% } -%>
 
 @NgModule({
   declarations: [
@@ -14,8 +19,24 @@ import { CapLiveChatModule } from './modules/cap-live-chat/cap-live-chat.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    <% if (imports && imports.liveChat) { %>
+    HttpClientModule,<%if(authService === 'auth0'){%>
+    CapAuthModule.forRoot({
+      clientId: '<%- credentials ? credentials.AUTH0_CLIENT_ID : "" %>',
+      clientSecret: '<%- credentials ? credentials.AUTH0_CLIENT_SECRET : "" %>',
+      domain: '<%- credentials ? credentials.AUTH0_DOMAIN : "" %>'
+    }),<% } %><% if (authService === 'firebase') { %>
+    CapAuthModule,
+    AngularFireModule.initializeApp({
+      apiKey: '<%- credentials ? credentials.apiKey : "" %>',
+      authDomain: '<%- credentials ? credentials.authDomain : "" %>',
+      databaseURL:'<%- credentials ? credentials.databaseURL : "" %>',
+      projectId: '<%- credentials ? credentials.projectId : "" %>',
+      storageBucket: '<%- credentials ? credentials.storageBucket : "" %>',
+      messagingSenderId: '<%- credentials ? credentials.messagingSenderId : "" %>',
+      appId: '<%- credentials ? credentials.appId : "" %>',
+      measurementId:'<%- credentials ? credentials.measurementId : "" %>'
+    }),
+    AngularFireAuthModule,<% } %><% if (imports && imports.liveChat) { %>
     CapLiveChatModule.forRoot({
       embeddedServiceName: 'liveChat-embeddedServiceName',
       idServiceName: 'liveChat-idServiceName',
@@ -26,7 +47,8 @@ import { CapLiveChatModule } from './modules/cap-live-chat/cap-live-chat.module'
       buttonId: 'liveChat-buttonId',
       baseLiveAgentURL: 'liveChat-baseLiveAgentURL',
       scriptUrl: 'liveChat-scriptUrl',
-    })<% } -%>
+    }),<% } %><% if(imports && imports.awsStorage){%>
+    CapAwsModule,<% } %>
   ],
   providers: [],
   bootstrap: [AppComponent],
