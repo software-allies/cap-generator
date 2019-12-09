4f201e8294081fbd2420f0bec7f7752d0e4dd135
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-12-09 10:28:43 -0600

    CAP-58 CAP-62 generators/app/templates/client/src/app/modules/cap-auth/login/ Fix all bugs in the LogIn component.

--- generators/app/templates/client/src/app/modules/cap-auth/login/login.component.ts
+++ generators/app/templates/client/src/app/modules/cap-auth/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
   loginUserForm: FormGroup;
   userNotValid: boolean;
   socialMedia: boolean;
+  validatedForm: boolean;
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -28,45 +29,50 @@ export class LoginComponent implements OnInit {
     });
     this.userNotValid = false;
     this.socialMedia = false;
+    this.validatedForm = false;
   }
 
   ngOnInit() { }
 
-  loginUser() {<% if (authService === 'auth0')  { %>
-    this.authenticationService.loginUser(this.loginUserForm.value).subscribe((token: any) => {
-      this.authenticationService.getAuth0UserInfo(token.access_token).subscribe((user: any) => {
-        if (isPlatformBrowser(this.platformId)) {
-          localStorage.setItem('User', JSON.stringify({
-            user: user.name,
-            email: user.email,
-            refresh_token: token.refresh_token,
-            token: token.access_token,
-            token_id: token.id_token,
-            id: user.sub
-          }));
-          this.communicationComponentsService.sendUser(true);
-          this.router.navigate(['/']);
-        }
-      });
-    }, (error) => {
-      this.userNotValid = true;
-    });<% } %>
-    <% if (authService === 'firebase')  { %>
-    this.authenticationService.loginUser(this.loginUserForm.value)
-    .then((response) => {
-      response.user.getIdTokenResult().then((res) => {
-        if (isPlatformBrowser(this.platformId)) {
-          localStorage.setItem('User', JSON.stringify({
-            user: response.user.email.split('@', 1)[0],
-            email: response.user.email,
-            refresh_token: response.user.refreshToken,
-            token: res.token
-          }));
-          this.communicationComponentsService.sendUser(true);
-          this.router.navigate(['/']);
-        }
-      });
-    }).catch(error => this.userNotValid = true); <%}%>
+  loginUser() {
+    if (this.loginUserForm.valid) {<% if (authService === 'auth0')  { %>
+      this.authenticationService.loginUser(this.loginUserForm.value).subscribe((token: any) => {
+        this.authenticationService.getAuth0UserInfo(token.access_token).subscribe((user: any) => {
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('User', JSON.stringify({
+              user: user.name,
+              email: user.email,
+              refresh_token: token.refresh_token,
+              token: token.access_token,
+              token_id: token.id_token,
+              id: user.sub
+            }));
+            this.communicationComponentsService.sendUser(true);
+            this.router.navigate(['/']);
+          }
+        });
+      }, (error) => {
+        this.userNotValid = true;
+      });<% } %>
+      <% if (authService === 'firebase')  { %>
+      this.authenticationService.loginUser(this.loginUserForm.value)
+      .then((response) => {
+        response.user.getIdTokenResult().then((res) => {
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('User', JSON.stringify({
+              user: response.user.email.split('@', 1)[0],
+              email: response.user.email,
+              refresh_token: response.user.refreshToken,
+              token: res.token
+            }));
+            this.communicationComponentsService.sendUser(true);
+            this.router.navigate(['/']);
+          }
+        });
+      }).catch(error => this.userNotValid = true); <%}%>
+    } else {
+      this.validatedForm = true;
+    }
   }
 
   signInSocialMedia(socialMedia: boolean) {
