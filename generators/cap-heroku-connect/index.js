dc8a110eab1d53f6ad8f4c09009d4ea558e0d1ac
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-07 12:30:36 -0600

    CAP-230 generators/cap-heroku-connect/index.js Fix a bug for Windows OS, implement more validations and functionalities so that the API works only with what is necessary and not with the authentication module that comes by default.

--- generators/cap-heroku-connect/index.js
+++ generators/cap-heroku-connect/index.js
@@ -56,7 +56,7 @@ module.exports = class extends Generator {
    * @returns
    */
   writing() {
-    const child = cp.spawn("/bin/sh", ["-c", `lb ${this.props.path} --skip-install`], { cwd: "" });
+    const child = cp.spawn("bash", ["-c", `lb ${this.props.path} --skip-install`], { cwd: "" });
     switch (this.props.sync) {
       case 'CustomSync': {
         console.log("We are working on it");
@@ -66,17 +66,16 @@ module.exports = class extends Generator {
         exec("lb --version", (error, stdout, stderr) => {
           if (error) {
             console.log(`name: ${error.name}\nMessage: ${error.message}\nStack: ${error.stack}`);
-            console.log("no tienes instalado Loopback-cli, enseguida lo instalaremos");
+            console.log("you don't have Loopback-cli installed, we will install it right away");
             exec("npm install -g loopback-cli",(error, stdout, stderr) => {
               if (error) {
                 console.log(`name: ${error.name}\nMessage: ${error.message}\nStack: ${error.stack}`);
                 console.log("Loopback could not be installed, try later");
                 return;
               } else {
-                // console.log(stdout);
+                // console.log('stdout',stdout);
                 console.log("Loopback was installed correctly, we will proceed to create a RESt API");
-
-                child.stdout.on("data", (data) => child.stdin.write("\n"));
+                child.stdout.on("data", (data) => { child.stdin.write("\n") });
                 child.stderr.on("data", (data) => { console.log('warning: incompatibility error')});
                 child.on('exit', (code) => {
                   if (code === 0) {
@@ -98,11 +97,11 @@ module.exports = class extends Generator {
                       {}
                     )
                     const tsParser = new Parser();
-                    const tsParceDB = new Parser();
+                    const tsParseDB = new Parser();
                     tsParser.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`)); // First add the file to the virtual folder the plugin creates
-                    tsParceDB.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
+                    tsParseDB.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
                     const file = tsParser.getSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`)); // Then we read and parse that file to AST
-                    const fileDB = tsParceDB.getSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
+                    const fileDB = tsParseDB.getSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
                     const importsModels =
                     /"Role": {/g;
                     const dataSource = /"connector": "memory"/g;
@@ -140,9 +139,8 @@ module.exports = class extends Generator {
               }
             });
           } else if (stdout) {
-
             console.log("You have loopback installed, we will proceed to create a REST API");
-            child.stdout.on("data", (data) => child.stdin.write("\n"));
+            child.stdout.on("data", (data) => { child.stdin.write("\n") });
             child.stderr.on("data", (data) => { console.log('warning: incompatibility error')});
             child.on('exit', (code) => {
               if (code === 0) {
@@ -150,32 +148,122 @@ module.exports = class extends Generator {
                   if (error) {
                     console.log('error: error installing postgres dependency')
                   } else if (stdout){
-                    console.log('Postgres dependency installed');
+                    console.log('Postgres dependency installed.');
                   }
                 });
-                console.log(`Your REST API has been successfully created ${code}`);
+                console.log(`Your REST API has been successfully created ${code} .`);
+                /**
+                 * copyTPL START
+                 */
                 this.fs.copyTpl(
-                  this.templatePath('cap-heroku-connect-api/**'),
+                  this.templatePath('cap-heroku-connect-api/models/**'),
                   this.destinationPath(`${this.props.path}`),
                   {}
+                );
+                this.fs.copyTpl(
+                  this.templatePath('cap-heroku-connect-api/auth/**'),
+                  this.destinationPath(`${this.props.path}/server/`),
+                  {
+                    domain: this.options.AuthDomain
+                  }
                 );
                 this.fs.copyTpl(
                   this.templatePath('cap-heroku-connect-web/**'),
                   this.destinationPath(`${this.options.name}/src/app/modules/`),
                   {}
-                )
+                );
+
+                /**
+                 * model-config Delete
+                 */
                 const tsParser = new Parser();
-                const tsParceDB = new Parser();
-                tsParser.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`)); // First add the file to the virtual folder the plugin creates
-                tsParceDB.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
-                const file = tsParser.getSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`)); // Then we read and parse that file to AST
-                const fileDB = tsParceDB.getSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));
-                const importsModels =
-                /"Role": {/g;
+                tsParser.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`));
+                const file = tsParser.getSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`));
+                file.removeText(file.getPos(), file.getEnd());
+                file.saveSync();
+
+                /**
+                 * authentication.js Delete
+                 */
+                const tsParserAuth = new Parser();
+                tsParserAuth.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/boot/authentication.js`))
+                const fileDelete = tsParserAuth.getSourceFile(this.destinationPath(`${this.props.path}/server/boot/authentication.js`));
+                fileDelete.removeText(fileDelete.getPos(), fileDelete.getEnd());
+                fileDelete.saveSync();
+
+                /**
+                 * DataSource-file
+                 */
+                const tsParseDB = new Parser();
+                tsParseDB.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`));  // First add the file to the virtual folder the plugin creates
+                const fileDB = tsParseDB.getSourceFile(this.destinationPath(`${this.props.path}/server/datasources.json`)); // Then we read and parse that file to AST
                 const dataSource = /"connector": "memory"/g;
-                const newText = file.getText()
-                .replace(importsModels,
-  `"Lead": {
+                const newTextDB = fileDB.getText().replace(dataSource,
+    `"url": "${this.props.dataBase}?ssl=true",
+    "connector": "postgresql"`);
+                fileDB.removeText(fileDB.getPos(), fileDB.getEnd());  // Remove all the text since we already have the text formed with the correct values
+                fileDB.insertText(0,newTextDB); // Insert new text
+                fileDB.saveSync();  // Save all changes
+
+                /**
+                 * middleware.json
+                 */
+                const tsParseAuth = new Parser ();
+                tsParseAuth.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/middleware.json`));
+                const fileAuth = tsParseAuth.getSourceFile(this.destinationPath(`${this.props.path}/server/middleware.json`));
+                const authMiddleware = /"auth": {},/g;
+                const restApiRoot = "${restApiRoot}";
+                const newTextAuth = fileAuth.getText()
+                .replace(authMiddleware,
+  `"auth": {
+    "./auth.js":{
+      "paths": [
+        "${restApiRoot}"
+      ]
+    }
+  },`);
+                fileAuth.removeText(fileAuth.getPos(), fileAuth.getEnd());
+                fileAuth.insertText(0, newTextAuth);
+                fileAuth.saveSync();
+
+                /**
+                 * API-dependencies
+                 */
+                const tsParseDependencies = new Parser ();
+                tsParseDependencies.addExistingSourceFile(this.destinationPath(`${this.props.path}/package.json`));
+                const fileDependencies = tsParseDependencies.getSourceFile(this.destinationPath(`${this.props.path}/package.json`));
+                const dependencies = /"dependencies": {/g;
+                const newTextDependencies = fileDependencies.getText().replace(dependencies,
+  `"dependencies": {
+    "express-jwt": "^5.3.1",
+    "jwks-rsa": "^1.6.0",`);
+                fileDependencies.removeText(fileDependencies.getPos(), fileDependencies.getEnd());
+                fileDependencies.insertText(0, newTextDependencies);
+                fileDependencies.saveSync();
+
+                /**
+                 * model-config.json
+                 */
+                const tsParserModelConfig = new Parser();
+                tsParserModelConfig.addExistingSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`));
+                const fileModelConfig = tsParserModelConfig.getSourceFile(this.destinationPath(`${this.props.path}/server/model-config.json`));
+                fileModelConfig.insertText(0,
+`{
+  "_meta": {
+    "sources": [
+      "loopback/common/models",
+      "loopback/server/models",
+      "../common/models",
+      "./models"
+    ],
+    "mixins": [
+      "loopback/common/mixins",
+      "loopback/server/mixins",
+      "../common/mixins",
+      "./mixins"
+    ]
+  },
+  "Lead": {
     "dataSource": "db",
     "public": true
   },
@@ -190,18 +278,9 @@ module.exports = class extends Generator {
   "Opportunity": {
     "dataSource": "db",
     "public": true
-  },
-  "Role": {`);
-                file.removeText(file.getPos(), file.getEnd()); // Remove all the text since we already have the text formed with the correct values
-                file.insertText(0, newText); // Insert new text
-                file.saveSync(); // Save all changes
-
-                const newTextDB = fileDB.getText().replace(dataSource,
-    `"url": "${this.props.dataBase}?ssl=true",
-    "connector": "postgresql"`)
-                fileDB.removeText(fileDB.getPos(), fileDB.getEnd());
-                fileDB.insertText(0,newTextDB);
-                fileDB.saveSync();
+  }
+}`);
+                fileModelConfig.saveSync()
               }
             });
           }
