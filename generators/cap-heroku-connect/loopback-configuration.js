07cd2ea35428e0356a99347c28fbceac8ce65b3b
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-13 13:16:37 -0600

    CAP-250 generators/cap-heroku-connect/loopback-configuration.js Create a new file to refactor the code we have in the index.js so that it is a bit more modularized.

---
+++ generators/cap-heroku-connect/loopback-configuration.js
@@ -0,0 +1,13 @@
+const { exec } = require('promisify-child-process');
+
+const loopbackCLI = async () => {
+  try {
+
+  } catch (error) {
+    console.log('error: ', error);
+  }
+}
+
+module.exports = {
+  loopbackCLI
+};
