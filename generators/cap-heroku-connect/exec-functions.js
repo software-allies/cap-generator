7a5388e0f02b1a3cf363c43bfd83ece0462df8db
Diego SanMo <diego14290764@itcg.edu.mx>
2020-01-14 17:08:56 -0600

    The exec-function file was added to contain all the exec functions related with Heroku.

---
+++ generators/cap-heroku-connect/exec-functions.js
@@ -0,0 +1,62 @@
+const { exec, spawn } = require('promisify-child-process');
+
+const herokuInstallation = async () => exec('npm install -g heroku');
+const herokuVersionSpawn = async () => spawn('heroku --version');
+const herokuVersion = async () => exec('heroku --version');
+const herokuConnectVerification = async () => exec('heroku plugins');
+const herokuConnectInstallation = async () =>
+  exec('heroku plugins:install heroku-connect-plugin');
+
+// Start a new project
+const herokuCreateApp = async name => exec(`heroku apps:create ${name}`);
+const hrkCreatePostgreSql = async name =>
+  exec(`heroku addons:create heroku-postgresql -a ${name}`);
+
+const herokuCredentials = async name =>
+  exec(`heroku pg:credentials:url DATABASE_URL -a ${name}`);
+
+const herokuConnectCreation = async name =>
+  exec(`heroku addons:create herokuconnect -a ${name}`);
+
+const setUpConnect = async command => exec(`${command}`);
+
+const authConnection = async name =>
+  exec(`heroku authorizations:create --description "For use with ${name}"`);
+
+const tokenApplication = async () => exec(`heroku auth:token`);
+
+const curlPost = async data =>
+  exec(
+    `curl -X POST -H "Authorization: Bearer ${
+      data.token
+    }" https://hc-central.heroku.com/auth/${data.name}`
+  );
+
+const schemaConnection = async name =>
+  exec(`heroku connect:db:set --schema salesforce --db DATABASE_URL -a ${name}`);
+
+const salesforceAuth = async name => exec(`heroku connect:sf:auth -a ${name}`);
+
+const mapping = async data => {
+  console.log('data: ', data);
+  exec(`heroku connect:import ${data.path}/heroku-config.json -a ${data.name}`);
+};
+
+module.exports = {
+  herokuVersionSpawn,
+  herokuVersion,
+  herokuInstallation,
+  herokuConnectVerification,
+  herokuConnectInstallation,
+  herokuCreateApp,
+  hrkCreatePostgreSql,
+  herokuCredentials,
+  herokuConnectCreation,
+  setUpConnect,
+  authConnection,
+  tokenApplication,
+  curlPost,
+  schemaConnection,
+  salesforceAuth,
+  mapping
+};
