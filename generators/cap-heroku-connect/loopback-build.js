efc38cae4ea30f395c26a8b081c9c7c7b2a87c0c
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-14 11:36:08 -0600

    CAP-250 generators/cap-heroku-connect/loopback-build.js Install loopback if it is not installed.

--- generators/cap-heroku-connect/loopback-build.js
+++ generators/cap-heroku-connect/loopback-build.js
@@ -7,8 +7,18 @@ async function loopbackBuild (appName) {
   const { stdout, stderr, code } = await child;
 }
 
-const loopbackCLI = async (appName) => {
+async function installLoopback () {
+  const child = exec('npm install -g loopback-cli', {});
+  child.stdout.on('data', (data) => console.log(data));
+  child.stderr.on('data', (data) => console.log(data));
+  const { stdout, stderr, code } = await child;
+}
+
+const loopbackCLI = async (appName, installation) => {
   try {
+    if (installation) {
+      await installLoopback();
+    }
     await loopbackBuild(appName);
   } catch (error) {
     console.log('error: ', error);
