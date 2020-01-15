22f4bb472dea0d0863c1bf0257b9be1a4dd030e7
Diego SanMo <diego14290764@itcg.edu.mx>
2020-01-15 10:59:04 -0600

    Removing blank space

--- generators/cap-heroku-connect/exec-functions.js
+++ generators/cap-heroku-connect/exec-functions.js
@@ -46,7 +46,8 @@ const schemaConnection = async name =>
 
 const salesforceAuth = async name => exec(`heroku connect:sf:auth -a ${name}`);
 
-const mapping = async data => exec(`heroku connect:import ${data.path}/heroku-config.json -a ${data.name}`);
+const mapping = async data =>
+  exec(`heroku connect:import ${data.path}/heroku-config.json -a ${data.name}`);
 
 module.exports = {
   herokuVersionSpawn,
