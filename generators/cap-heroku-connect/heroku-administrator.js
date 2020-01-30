cffc85e4335e7eec585ebef976e8ad12353f64d7
Diego SanMo <diego14290764@itcg.edu.mx>
2020-01-30 10:16:20 -0600

    The logic and the structure was changed.

--- generators/cap-heroku-connect/heroku-administrator.js
+++ generators/cap-heroku-connect/heroku-administrator.js
@@ -4,9 +4,11 @@ let load = null;
 
 exports.run = (promise, messages, appName) => {
   let commandResult;
+  let errorAction;
   return new Promise(async (resolve, reject) => {
     try {
-      load = loading(messages.actionMessage.blue).start();
+      load = loading(messages.action_message.blue).start();
+
       if (appName) {
         commandResult = await promise(appName);
       } else {
@@ -15,41 +17,56 @@ exports.run = (promise, messages, appName) => {
 
       if (commandResult.stdout === 'no plugins installed\n') {
         load.stop();
-        load.fail(messages.errorMessage);
-        let err = {
+        load.fail(messages.error_message);
+        errorAction = {
           messages: commandResult.stdout,
           code: 400,
           description: 'Heroku Connect is not installed'
         };
-        reject(err);
+        reject(errorAction);
       } else {
         load.stop();
-        load.succeed(messages.responseMessage);
+        load.succeed(messages.response_message);
         resolve(commandResult);
       }
     } catch (error) {
-      console.log('error from testing: ', error);
-      // Reject when the user is not find it
-      // Error when the user doesn't have installed Heroku's CLI
-      if (error.code === 1) {
-        load.stop();
-        load.fail(messages.error_message);
-        let err = {
-          messages: 'Heroku CLI is not installed',
-          code: 1,
-          description: "You don't have installed Heroku CLI"
-        };
-        reject(err);
-      }
-      if (error.code === 100) {
-        load.stop();
-        load.fail(messages.error_message);
-        let err = {
-          messages: 'User not found',
-          code: 100,
-          description: "You don't access"
-        };
-        reject(err);
+      console.log('error: ', error);
+      switch (error.code) {
+        case 1:
+          load.stop();
+          load.fail('The application name is already taken.');
+          errorAction = {
+            messages: `${error.stderr}`,
+            code: 1,
+            description: 'The application name is already taken.'
+          };
+          reject(errorAction);
+          break;
+
+        // Reject when the user is not find it
+        case 100:
+          load.stop();
+          load.fail(messages.error_message);
+          errorAction = {
+            messages: 'User not found',
+            code: 100,
+            description: "You don't access"
+          };
+          reject(errorAction);
+          break;
+        case 127:
+          load.stop();
+          load.fail(messages.error_message);
+          errorAction = {
+            description: "Heroku's CLI is not installed.",
+            messages: "Heroku's CLI is not installed.",
+            code: 127
+          };
+          reject(errorAction);
+          break;
+
+        default:
+          break;
       }
     }
   });
--- generators/cap-heroku-connect/heroku-connect.js
+++ generators/cap-heroku-connect/heroku-connect.js
@@ -1,16 +1,45 @@
 const herokuService = require('./heroku-administrator');
 const command = require('./exec-functions');
 const loadMessages = require('./load-messages');
+const prompts = require('prompts');
+let herokuConfiguration = {};
 
-exports.herokuCLI = async (appName, path) => {
+const verifyInstallation = async () => {
   try {
     await herokuService.run(command.herokuVersion, loadMessages.herokuV);
-
     await herokuService.run(command.herokuConnectVerification, loadMessages.herokuC);
-
     await herokuService.run(command.checkUser, loadMessages.checkUser);
 
-    let herokuConfiguration = {};
+  } catch (error) {
+    switch (error.code) {
+      case 100:
+        await herokuService.run(command.login, loadMessages.login);
+        break;
+      case 127:
+        await herokuService.run(
+          command.herokuInstallation,
+          loadMessages.herokuCLIInstallation
+        );
+        await herokuService.run(
+          command.herokuConnectInstallation,
+          loadMessages.herokuConnectIns
+        );
+        await herokuService.run(command.login, loadMessages.login);
+        break;
+      // Code 400
+      default:
+        await herokuService.run(
+          command.herokuConnectInstallation,
+          loadMessages.herokuConnectIns
+        );
+        await herokuService.run(command.login, loadMessages.login);
+        break;
+    }
+  }
+};
+
+const appsCreation = async appName => {
+  try {
     let urls = await herokuService.run(
       command.herokuCreateApp,
       loadMessages.herokuCreateApp,
@@ -25,34 +54,71 @@ exports.herokuCLI = async (appName, path) => {
       loadMessages.herokuCreatePostgres,
       appName
     );
+  } catch (error) {
+    let isNotTheValidName = true;
+    let promptResponse;
+    while (isNotTheValidName) {
+      // eslint-disable-next-line no-await-in-loop
+      promptResponse = await prompts({
+        type: 'text',
+        name: 'newName',
+        message: 'New name: ',
+        validate: newName => newName.length < 5 ? 'You wrote a name with less than 5 characters' : true
+      });
 
+      // eslint-disable-next-line no-negated-condition
+      if (promptResponse.newName !== appName) isNotTheValidName = false;
+      else
+        console.log(`The ${promptResponse.newName} is already taken, write another one`);
+    }
+    if (promptResponse) {
+      let urls = await herokuService.run(
+        command.herokuCreateApp,
+        loadMessages.herokuCreateApp,
+        appName
+      );
+      let herokUrls = urls.stdout.split('|');
+      herokuConfiguration.herokuURL = herokUrls[0];
+      herokuConfiguration.herokuGit = herokUrls[1];
+      await herokuService.run(
+        command.hrkCreatePostgreSql,
+        loadMessages.herokuCreatePostgres,
+        promptResponse.newName
+      );
+      return promptResponse.newName;
+    }
+  }
+};
+
+const startConfigurationApp = async (name, path) => {
+  try {
     let credentials = await herokuService.run(
       command.herokuCredentials,
       loadMessages.herokuCredentials,
-      appName
+      name
     );
 
     let startPosition = credentials.stdout.search('postgres');
-
-    // DBURL it's going to save the url that it's going to be in Loopback
+    // // DBURL it's going to save the url that it's going to be saved in Loopback
     const DBURL = credentials.stdout.slice(startPosition, credentials.stdout.length - 1);
     herokuConfiguration.postgresURL = DBURL;
     let dbstatus = await herokuService.run(
       command.herokuConnectCreation,
       loadMessages.herokuConnectCreation,
-      appName
+      name
     );
 
-    let commandetupConnect = dbstatus.stdout.split('`')[1];
+    let commandSetupConnect = dbstatus.stdout.split('`')[1];
 
     await herokuService.run(
       command.setUpConnect,
       loadMessages.herokuConnectSetup,
-      commandetupConnect
+      commandSetupConnect
     );
-    // Let infoConnection = await run(command.authConnection, loadMessages.authConnection, appName)
-    // Let startPositionC = infoConnection.stdout.search('ID') + 13
-    // Let endPositionC = infoConnection.stdout.search('Description') - 1
+
+    // Let infoConnection = await herokuService.run(command.authConnection, loadMessages.authConnection, name)
+    // let startPositionC = infoConnection.stdout.search('ID') + 13
+    // let endPositionC = infoConnection.stdout.search('Description') - 1
 
     // var connection_id = infoConnection.stdout.slice(startPositionC, endPositionC);
 
@@ -62,7 +128,7 @@ exports.herokuCLI = async (appName, path) => {
     );
 
     let config = {
-      name: appName,
+      name: name,
       token: token.stdout
     };
 
@@ -71,20 +137,39 @@ exports.herokuCLI = async (appName, path) => {
     await herokuService.run(
       command.schemaConnection,
       loadMessages.schemaConnection,
-      appName
+      name
     );
 
-    await herokuService.run(command.salesforceAuth, loadMessages.salesforceAuth, appName);
+    await herokuService.run(command.salesforceAuth, loadMessages.salesforceAuth, name);
 
     let map = {
       path: path,
-      name: appName
+      name: name
     };
-    // Await run(command.schemaConnection, loadMessages.schemaConnection, appName)
-    await herokuService.run(command.mapping, loadMessages.mapping, map);
-    return herokuConfiguration;
+
+    await herokuService.run(
+      command.schemaConnection,
+      loadMessages.schemaConnection,
+      name
+    );
+    await herokuService.run(command.mapping, loadMessages.mapping, map)
   } catch (error) {
     console.log('error: ', error);
+  }
+};
+
+exports.herokuCLI = async (appName, path) => {
+
+  try {
+    await verifyInstallation();
+    let newName = await appsCreation(appName);
+
+    // eslint-disable-next-line no-unused-expressions
+    newName
+      ? await startConfigurationApp(newName, path)
+      : await startConfigurationApp(appName, path);
+    return herokuConfiguration;
+  } catch (error) {
     if (error.description === 'Heroku Connect is not installed') {
       await herokuService.run(
         command.herokuConnectInstallation,
@@ -93,15 +178,6 @@ exports.herokuCLI = async (appName, path) => {
     }
     if (error.code === 100) {
       await herokuService.run(command.login, loadMessages.login);
-    }
-
-    if (error.code === 1) {
-      let install = await herokuService.run(
-        command.herokuInstallation,
-        loadMessages.herokuCLIInstallation
-      );
-
-      console.log('install: ', install);
     }
   }
 };
