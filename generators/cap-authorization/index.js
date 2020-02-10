ad1cdfa1331a6ed7101d97174a822dac2c28d019
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-10 16:33:43 -0600

    CAP-310 generators/cap-authorization/index.js Implement authentication schematics to be called inside the CAP generator.

--- generators/cap-authorization/index.js
+++ generators/cap-authorization/index.js
@@ -1,13 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const Parser = require('ts-simple-ast').default;
 
 module.exports = class extends Generator {
 
    /**
    * @description Ask the user the configuration information for AUTH MODULE
-   * @author Christofer Flores <cristofer@sofwareallies.com>
+   * @author Lenin Emmanuel Espinoza <lenin_emmanuel@sofwareallies.com>
    * @returns
    */
   prompting() {
@@ -28,149 +27,34 @@ module.exports = class extends Generator {
             name: `Auth0`,
             value: 'auth0'
           }
-        ],
-      },
-      {
-        type: 'input',
-        name: 'AUTH0_CLIENT_ID',
-        message: 'Set your Auth0 Client ID: ',
-        default: '',
-        when: ctx => ctx.modules === 'auth0'
-      },
-      {
-        type: 'input',
-        name: 'AUTH0_CLIENT_SECRET',
-        message: 'Set your Auth0 Client Secret: ',
-        default: '',
-        when: ctx => ctx.modules === 'auth0'
-      },
-      {
-        type: 'input',
-        name: 'AUTH0_DOMAIN',
-        message: 'Set your Auth0 Domain: ',
-        default: '',
-        when: ctx => ctx.modules === 'auth0'
-      },
-      {
-        type: 'input',
-        name: 'apiKey',
-        message: 'Set your ApiKey: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'authDomain',
-        message: 'Set your Auth Domain: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'databaseURL',
-        message: 'Set your data base URL: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'projectId',
-        message: 'Set your Project ID: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'storageBucket',
-        message: 'Set your storage bucket: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'senderId',
-        message: 'Set your message sender ID: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'appId',
-        message: 'Set your app ID: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
-      {
-        type: 'input',
-        name: 'measurementId',
-        message: 'Set your measurement ID: ',
-        default: '',
-        when: ctx => ctx.modules === 'firebase'
-      },
+        ]
+      }
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
    /**
    * @description Once we get the information update the module to include the configuration.
-   * @author Christofer Flores <cristofer@sofwareallies.com>
+   * @author Lenin Emmanuel Espinoza <lenin_emmanuel@sofwareallies.com>
    * @returns
    */
-  writing() {
-    const tsParser = new Parser()
-    tsParser.addExistingSourceFile(this.destinationPath(`${this.options.name}/src/app/app.module.ts`)); // First add the file to the virtual folder the plugin creates
-    const file = tsParser.getSourceFile(this.destinationPath(`${this.options.name}/src/app/app.module.ts`)); // Then we read and parse that file to AST
-    const apiUrlRgx = /apiUrl: 'apiUrl'/g
-    const endpointRgx = /loginEndpoint: 'loginEndPoint'/g
-    const newText =  file.getText()
-      .replace(apiUrlRgx, `apiUrl: '${this.props.authApiUrl}'`)
-      .replace(endpointRgx, `loginEndpoint: '${this.props.authLoginEndPoint}'`)
+  writing() {}
 
-    file.removeText(file.getPos(), file.getEnd()); // Remove all the text since we already have the text formed with the correct values
-
-    file.insertText(0, newText); // Insert new text
-
-    file.saveSync(); // Save all changes
-
+  install() {
+    this.log(`${chalk.bgGreen('Installing Authentication Module')}`);
     if (this.props.modules === 'auth0'){
-      this.fs.write(
-        this.destinationPath(`${this.options.name}/src/environments/environment.ts`),
-        `export const environment = {
-          production: false,
-          AUTH0_DOMAIN: '${this.props.AUTH0_DOMAIN}',
-          AUTH0_CLIENT_ID: '${this.props.AUTH0_CLIENT_ID}',
-          AUTH0_CLIENT_SECRET: '${this.props.AUTH0_CLIENT_SECRET}'
-        };`
-      )
+      this.spawnCommandSync('npm', ['i', '-g', 'cap-angular-schematic-auth-auth0']);
+
+      this.spawnCommandSync('ng',['g', 'cap-angular-schematic-auth-auth0:cap-angular-schematic-auth-auth0'], {cwd:this.destinationPath(this.options.name)});
+
     } else if (this.props.modules = 'firebase'){
-      this.fs.write(
-        this.destinationPath(`${this.options.name}/src/environments/environment.ts`),
-        `export const environment = {
-          production: false,
-          firebase: {
-            apiKey: '${this.props.apiKey}',
-            authDomain: '${this.props.authDomain}',
-            databaseURL: '${this.props.databaseURL}',
-            projectId: '${this.props.projectId}',
-            storageBucket: '${this.props.storageBucket}',
-            messagingSenderId: '${this.props.senderId}',
-            appId: '${this.props.appId}',
-            measurementId: '${this.props.measurementId}'
-          }
-        };`
-      )
+
+      this.spawnCommandSync('npm', ['i', '-g', 'cap-angular-schematic-auth-firebase']);
+
+      this.spawnCommandSync('ng', ['g', 'cap-angular-schematic-auth-firebase:cap-angular-schematic-auth-firebase'], {cwd:this.destinationPath(this.options.name)});
     }
-    // Finally just copy the pages
-    this.fs.copyTpl(
-      this.templatePath('cap-auth/**'),
-      this.destinationPath(`${this.options.name}/src/app/modules/`), {
-        name: this.options.name,
-        service: this.props.modules
-      }
-    );
   }
 }
