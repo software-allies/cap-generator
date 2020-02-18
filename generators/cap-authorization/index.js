62628045769cc2b72db52853caad8c18b5fd8a7f
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-18 11:51:40 -0600

    CAP-348 generators/cap-authorization/index.js Authentication module for an existing project.

--- generators/cap-authorization/index.js
+++ generators/cap-authorization/index.js
@@ -16,7 +16,7 @@ module.exports = class extends Generator {
     const prompts = [
       {
         type: 'list',
-        name: 'modules',
+        name: 'authService',
         message: 'Select the service you want to use to authenticate users:',
         choices: [
           {
@@ -28,7 +28,86 @@ module.exports = class extends Generator {
             value: 'auth0'
           }
         ]
-      }
+      },
+      {
+        type: 'input',
+        name: 'AUTH0_CLIENT_ID',
+        message: 'Set your Auth0 Client ID: ',
+        default: '',
+        when: ctx => ctx.authService === 'auth0'
+      },
+      {
+        type: 'input',
+        name: 'AUTH0_CLIENT_SECRET',
+        message: 'Set your Auth0 Client Secret: ',
+        default: '',
+        when: ctx => ctx.authService === 'auth0'
+      },
+      {
+        type: 'input',
+        name: 'AUTH0_DOMAIN',
+        message: 'Set your Auth0 Domain: ',
+        default: '',
+        when: ctx => ctx.authService === 'auth0'
+      },
+
+
+      {
+        type: 'input',
+        name: 'apiKey',
+        message: 'Set your ApiKey: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'authDomain',
+        message: 'Set your Auth Domain: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'databaseURL',
+        message: 'Set your data base URL: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'projectId',
+        message: 'Set your Project ID: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'storageBucket',
+        message: 'Set your storage bucket: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'senderId',
+        message: 'Set your message sender ID: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'appId',
+        message: 'Set your app ID: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
+      {
+        type: 'input',
+        name: 'measurementId',
+        message: 'Set your measurement ID: ',
+        default: '',
+        when: ctx => ctx.authService === 'firebase'
+      },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -44,15 +123,45 @@ module.exports = class extends Generator {
   writing() {}
 
   install() {
-    this.log(`${chalk.bgGreen('Installing Authentication Module')}`);
-    if (this.props.modules === 'auth0'){
+    // this.log(`${chalk.bgGreen('Installing Authentication Module')}`);
 
-      this.spawnCommandSync('ng',['add', 'cap-angular-schematic-auth-auth0'], {cwd:this.destinationPath(this.options.name)});
+    if (this.props.authService === 'auth0'){
 
-    } else if (this.props.modules = 'firebase'){
+      this.spawnCommandSync(
+        'ng',
+        [
+          'add',
+          'cap-angular-schematic-auth-auth0',
+          `--clientID=${this.props.AUTH0_CLIENT_ID}`,
+          `--clientSecret=${this.props.AUTH0_CLIENT_SECRET}`,
+          `--domain=${this.props.AUTH0_DOMAIN}`,
+          `--endPoint=`
+        ],
+        {
+          cwd:this.destinationPath(this.options.name)
+        }
+      );
 
-      this.spawnCommandSync('ng', ['add', 'cap-angular-schematic-auth-firebase'], {cwd:this.destinationPath(this.options.name)});
+    } else if (this.props.authService = 'firebase'){
 
+      this.spawnCommandSync(
+        'ng',
+        [
+          'add',
+          'cap-angular-schematic-auth-firebase',
+          `--apiKey=${this.props.apiKey}`,
+          `--authDomain=${this.props.authDomain}`,
+          `--databaseURL=${this.props.databaseURL}`,
+          `--projectId=${this.props.projectId}`,
+          `--storageBucket=${this.props.storageBucket}`,
+          `--senderId=${this.props.senderId}`,
+          `--appId=${this.props.appId}`,
+          `--measurementId=${this.props.measurementId}`
+        ],
+        {
+          cwd:this.destinationPath(this.options.name)
+        }
+      );
     }
   }
 }
