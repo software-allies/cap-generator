51194d83ba422a2fa8a4b1fd3bb1d244e0539b31
Diego SanMo <diego14290764@itcg.edu.mx>
2020-02-19 13:15:52 -0600

    The CAP AWS S3 schematic was added into the CAP Generator.

--- generators/cap-storage-aws/index.js
+++ generators/cap-storage-aws/index.js
@@ -1,10 +1,8 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const Parser = require('ts-simple-ast').default;
 
 module.exports = class extends Generator {
-
   /**
    * @description Ask the user the configuration information for AWS STORAGE MODULE
    * @author Christofer Flores <cristofer@sofwareallies.com>
@@ -12,7 +10,10 @@ module.exports = class extends Generator {
    */
   prompting() {
     // Have Yeoman greet the user.
-    this.log(`=========================================\nNow lets configure the ${chalk.blue('AWS STORAGE MODULE')}\n==========================================
+    this
+      .log(`=========================================\nNow lets configure the ${chalk.blue(
+    'AWS STORAGE MODULE'
+    )}\n==========================================
     `);
     const prompts = [
       {
@@ -45,8 +46,7 @@ module.exports = class extends Generator {
         message: 'S3 Folder name?',
         default: '<aws-folder>'
       }
-    ]
-
+    ];
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
@@ -55,36 +55,23 @@ module.exports = class extends Generator {
 
   /**
    * @description Once we get the information update the module to include the configuration.
-   * @author Christofer Flores <cristofer@sofwareallies.com>
+   * @author Diego Moreno <diego@sofwareallies.com>
    * @returns
    */
   writing() {
-    /*const tsParser = new Parser()
-    tsParser.addExistingSourceFile(this.destinationPath(`${this.options.name}/src/app/app.module.ts`)); // First add the file to the virtual folder the plugin creates
-    const file = tsParser.getSourceFile(this.destinationPath(`${this.options.name}/src/app/app.module.ts`)); // Then we read and parse that file to AST
-    const bucketRgx = /bucket: 'aws-bucket'/g
-    const keyRgx = /accessKeyId: 'aws-key'/g
-    const secretRgx = /secretAccessKey: 'aws-secret'/g
-    const regionRgx = /region: 'aws-region'/g
-    const folderRgx = /folder: 'aws-folder'/g
-    const newText =  file.getText()
-      .replace(bucketRgx, `bucket: '${this.props.awsBucket}'`)
-      .replace(keyRgx, `accessKeyId: '${this.props.awsAccessKeyId}'`)
-      .replace(secretRgx, `secretAccessKey: '${this.props.awsSecretAccessKey}'`)
-      .replace(regionRgx, `region: '${this.props.awsRegion}'`)
-      .replace(folderRgx, `folder: '${this.props.awsFolder}'`)
-
-    file.removeText(file.getPos(), file.getEnd()); // Remove all the text since we already have the text formed with the correct values
-    file.insertText(0, newText); // Insert new text
-    file.saveSync(); // Save all changes*/
-
-    // Finally just copy the pages
-    this.fs.copyTpl(
-      this.templatePath('cap-aws/**'),
-      this.destinationPath(`${this.options.name}/src/app/modules/`),
+    this.spawnCommandSync(
+      'ng',
+      [
+        'add',
+        'cap-angular-schematic-storage-aws',
+        `--bucket=${this.props.awsBucket}`,
+        `--accessKeyId=${this.props.awsAccessKeyId}`,
+        `--secretAccessKey=${this.props.awsSecretAccessKey}`,
+        `--region=${this.props.awsRegion}`,
+        `--folder=${this.props.awsFolder}`
+      ],
       {
-        name: this.options.name,
-        credentials: this.props
+        cwd: this.destinationPath(this.options.name)
       }
     );
   }
