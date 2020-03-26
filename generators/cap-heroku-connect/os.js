7befb52ffa410d40c825eab7fdaed0285e07c161
Diego <diego14290764@itcg.edu.mx>
2020-03-26 15:59:27 -0600

    Adding the logic to create the json from Windows

--- generators/cap-heroku-connect/os.js
+++ generators/cap-heroku-connect/os.js
@@ -2,38 +2,51 @@ const { exec } = require('promisify-child-process');
 const https = require('https');
 const opsys = process.platform;
 const loading = require('loading-cli');
-
+const color = require('colors-cli/toxic');
 let load = null;
 
 let versionCommand = '';
 let installJQCommand = '';
+jwtCommandWindows = `curl -X PUT -d " https://www.googleapis.com/service_accounts/v1/metadata/x509/securetoken@system.gserviceaccount.com "| jq '[ to_entries | .[] | {alg: "RS256", kty: "RSA", use: "sig", kid: .key, x5c: [(.value | sub(".*"; "") | sub("\n"; ""; "g") | sub("-.*"; "")) ] } ] | {"keys": .}'`
 let jwtCommand = `curl -s 'https://www.googleapis.com/service_accounts/v1/metadata/x509/securetoken@system.gserviceaccount.com' | jq '[ to_entries | .[] | {alg: "RS256", kty: "RSA", use: "sig", kid: .key, x5c: [(.value | sub(".*"; "") | sub("\n"; ""; "g") | sub("-.*"; "")) ] } ] | {"keys": .}'`;
 
 const createJWTFirebase = async () => {
   try {
     load = loading('Creating a JSON to Firebase auth...'.blue).start();
-    let jwt = await exec(jwtCommand);
+if(opsys === 'darwin' || opsys === 'linux'){
+  let jwt = await exec(jwtCommand);
+  let jsonJWT = JSON.stringify(JSON.parse(jwt.stdout));
+  load.stop();
+  load.succeed('The JSON was successful generated.');
+  return jsonJWT;
+} else {
+  let jwt = await exec(jwtCommandWindows);
     let jsonJWT = JSON.stringify(JSON.parse(jwt.stdout));
     load.stop();
     load.succeed('The JSON was successful generated.');
     return jsonJWT;
+}
+    
   } catch (error) {
+    console.log(error)
     load.stop();
     load.fail('Error trying to generate the JSON.');
     console.log('error: ', error);
   }
 };
 
-const installJq = async command => {
+const installJq = async (command) => {
   try {
     load = loading('Installing jq...'.blue).start();
     let installationStatus = await exec(command);
-    if (installationStatus.stdout.includes('Already downloaded')) {
+    console.log(installationStatus)
+    if (installationStatus.stdout.includes('Already downloaded') || installationStatus.stdout.includes('Downloaded')) {
       load.stop();
       load.succeed('Installation finished');
       createJWTFirebase();
     }
   } catch (error) {
+    console.log(error)
     load.stop();
     load.fail('Error trying to install jq.');
     console.log('error: ', error);
@@ -44,6 +57,7 @@ const verifyJqVersion = async (versionCommand, installatioCommand) => {
   try {
     if (versionCommand === '') {
       console.log('its a windows machine');
+      
       installJq(installatioCommand);
     } else {
       let version = await exec(versionCommand);
@@ -52,6 +66,7 @@ const verifyJqVersion = async (versionCommand, installatioCommand) => {
       }
     }
   } catch (error) {
+    console.log(error)
     let errorStatus = {
       code: error.code,
       message: error.stderr
@@ -67,7 +82,7 @@ if (opsys === 'darwin' || opsys === 'linux') {
   verifyJqVersion(versionCommand, installJQCommand);
 } else {
   console.log('Windows');
-  installJQCommand = 'npm i node-jq -g';
+  installJQCommand = 'npm install node-jq -g';
   versionCommand = '';
   verifyJqVersion(versionCommand, installJQCommand);
 }
