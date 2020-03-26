08a0260dd3c3a79c96e0b37be04ff35cecb3c951
Diego SanMo <diego14290764@itcg.edu.mx>
2020-03-26 13:17:45 -0600

    Adding the loading feature to show the process of the creation of the JSON to firebase.

--- generators/cap-heroku-connect/os.js
+++ generators/cap-heroku-connect/os.js
@@ -1,6 +1,9 @@
 const { exec } = require('promisify-child-process');
 const https = require('https');
 const opsys = process.platform;
+const loading = require('loading-cli');
+
+let load = null;
 
 let versionCommand = '';
 let installJQCommand = '';
@@ -8,21 +11,31 @@ let jwtCommand = `curl -s 'https://www.googleapis.com/service_accounts/v1/metada
 
 const createJWTFirebase = async () => {
   try {
+    load = loading('Creating a JSON to Firebase auth...'.blue).start();
     let jwt = await exec(jwtCommand);
     let jsonJWT = JSON.stringify(JSON.parse(jwt.stdout));
+    load.stop();
+    load.succeed('The JSON was successful generated.');
     return jsonJWT;
   } catch (error) {
+    load.stop();
+    load.fail('Error trying to generate the JSON.');
     console.log('error: ', error);
   }
 };
 
 const installJq = async command => {
   try {
+    load = loading('Installing jq...'.blue).start();
     let installationStatus = await exec(command);
     if (installationStatus.stdout.includes('Already downloaded')) {
+      load.stop();
+      load.succeed('Installation finished');
       createJWTFirebase();
     }
   } catch (error) {
+    load.stop();
+    load.fail('Error trying to install jq.');
     console.log('error: ', error);
   }
 };
@@ -30,6 +43,7 @@ const installJq = async command => {
 const verifyJqVersion = async (versionCommand, installatioCommand) => {
   try {
     if (versionCommand === '') {
+      console.log('its a windows machine');
       installJq(installatioCommand);
     } else {
       let version = await exec(versionCommand);
