c0c7b13050c3f18e107de3c6a4028d6dfb0230f8
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-04-08 15:22:56 -0500

    CAP-411 generators/cap-live-chat/index.js Publish in the aws-storage credentials in the application created in heroku and also, attach a configuration file for .env for the moment of being deployed, the app will run a script to take the credentials of prosses.env

--- generators/cap-storage-aws/index.js
+++ generators/cap-storage-aws/index.js
@@ -1,6 +1,8 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
+const Parser = require('ts-simple-ast').default;
+const { exec, spawn } = require('promisify-child-process');
 
 module.exports = class extends Generator {
   /**
@@ -59,6 +61,48 @@ module.exports = class extends Generator {
    * @returns
    */
   writing() {
+    if (this.options.deployFrontEnd) {
+      this.env.arguments.push(
+        {key: 'AWS_BUCKET', value: this.props.awsBucket},
+        {key: 'AWS_ACCESS_KEY_ID', value: this.props.awsAccessKeyId},
+        {key: 'AWS_SECRET_ACCESS_KEY', value: this.props.awsSecretAccessKey},
+        {key: 'AWS_REGION', value: this.props.awsRegion},
+        {key: 'AWS_FOLDER', value: this.props.awsFolder},
+      )
+    }
+
+    const tsEnvironment = new Parser();
+    tsEnvironment.addExistingSourceFile(
+      this.destinationPath(
+        this.options.name ? `${this.options.name}/src/environments/environment.ts` : 'src/environments/environment.ts'
+      )
+    );
+
+    const fileEnvironment = tsEnvironment.getSourceFile(
+      this.destinationPath(
+        this.options.name ? `${this.options.name}/src/environments/environment.ts` : 'src/environments/environment.ts'
+      )
+    );
+
+    const environments = /export const environment = {/g;
+    const newEnvironments = fileEnvironment.getText().replace(environments,
+ `export const environment = {
+  bucket: '',
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: '',
+  folder: '',`);
+
+    fileEnvironment.removeText(fileEnvironment.getPos(), fileEnvironment.getEnd());
+    fileEnvironment.insertText(0, newEnvironments);
+    fileEnvironment.saveSync();
+
+    if (this.options.deployFrontEnd) {
+      this.env.arguments.map( async x => {
+        await exec(`heroku config:set ${x.key}=${x.value} --app=${this.options.angularHerokuApp}`);
+      });
+    }
+
     this.spawnCommandSync(
       'ng',
       [
