2f1f7ae5829a5f65f4e1986da88cb7ebb4bdcd1c
Diego SanMo <diego14290764@itcg.edu.mx>
2020-12-01 11:46:55 -0600

    The logic to install and verify the lb4 version has been changed.

---
+++ generators/cap-heroku-connect/loopback-4-installation.js
@@ -0,0 +1,40 @@
+const { exec } = require('promisify-child-process');
+const loading = require('loading-cli');
+
+let load = loading('Installing loopback 4');
+
+const lb4Version = async () => {
+  try {
+    load = loading('Searching an lb4 version already installed');
+    load.start();
+    let lb4Version = await exec('lb4 -v');
+    if (lb4Version.stderr === '') {
+      load.stop();
+      load.succeed('Loopback 4 is already installed.');
+      return true;
+    }
+  } catch (error) {
+    load.stop();
+    load.fail("There's not an lb4 version installed");
+    return false;
+  }
+};
+
+const lb4Installation = async () => {
+  try {
+    let existLb4 = await lb4Version();
+    if (existLb4 !== true) {
+      load = loading('Loopback 4 is installing...');
+      load.start();
+      await exec('npm i -g @loopback/cli');
+      load.stop();
+      load.succeed('Loopback 4 has been installed successfully');
+      return true;
+    }
+    return;
+  } catch (error) {
+    load.stop();
+    load.fail('Error trying to install lb4');
+  }
+};
+module.exports = lb4Installation;
